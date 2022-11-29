f61c5d66faccb150865512fe0dc4b85e0df8670b
MANI <maniadishu7342@gmail.com>
2022-11-29 19:44:43 +0530

    Emergency vehicle detection

---
+++ emergency.py
@@ -0,0 +1,103 @@
+from PIL import Image,ImageFilter
+
+import torchvision.transforms as transforms
+from torchvision import *
+from torch import *
+import cv2 as cv2
+import time
+
+from torchvision import *
+
+from torch import *
+
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.utils.data import Dataset, random_split, DataLoader
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import torch.nn as nn
+from torchvision.utils import make_grid
+
+
+# east_path="D:\\Centuriton\\models\\Test\\test1\\Emergency\\6.0.720.mp4"
+
+
+tick = time.time()
+class ImageClassificationBase(nn.Module):
+
+    def training_step(self, batch):
+        images, targets = batch
+        out = self(images)
+        # _,out = torch.max(out,dim = 1)
+        loss = F.binary_cross_entropy(torch.sigmoid(out), targets)
+        return loss
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_scores = [x['val_score'] for x in outputs]
+        epoch_score = torch.stack(batch_scores).mean()  # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_score': epoch_score.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_score: {:.4f}".format(
+            epoch, result['train_loss'], result['val_loss'], result['val_score']))
+
+
+class Densenet169(ImageClassificationBase):
+
+    def __init__(self):
+        super().__init__()
+        self.pretrained_model = models.densenet169(pretrained=True)
+
+        feature_in = self.pretrained_model.classifier.in_features
+        self.pretrained_model.classifier = nn.Linear(feature_in, 2)
+
+    def forward(self, x):
+        return self.pretrained_model(x)
+
+
+imsize = (224, 224)
+frames_to_skip = 10
+li = []
+im_output = "./"
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                                ])
+
+# loading the model
+loaded_densenet169 = Densenet169()
+loaded_densenet169.load_state_dict(torch.load('./' + 'densenet169.pt', map_location=torch.device('cpu')))
+loaded_densenet169.eval()
+
+
+def predict_emergency_vehicle(image_path):
+    uploaded_file = image_path
+
+    image = Image.open(uploaded_file).convert('RGB')
+
+    image = image.filter(ImageFilter.MedianFilter)
+
+    image = transform(image).view(1, 3, 224, 224)
+
+    pred = loaded_densenet169.forward(image)
+    proba, idx = torch.max(torch.sigmoid(pred), dim=1)
+
+    proba = proba.detach().numpy()[0]
+    idx = idx.numpy()[0]
+
+    print("Emergency_Vehicle") if idx == 1 else print("Non_Emergency Vehicle")
+    print("Confidence Level", float(proba))
+
+
+# video_reader_east = cv2.VideoCapture(east_path)
+
+
+
+predict_emergency_vehicle("Test/Test1.jpg")
+# predict_emergency_vehicle(im_output+"south.png")
+# predict_emergency_vehicle(im_output+"west.png")
+# predict_emergency_vehicle(im_output+"north.png")
+
+print('\n\n', 'Time taken: ', time.time() - tick)
